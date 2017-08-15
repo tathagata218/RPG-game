4ee9fb2f1ef4f2ac9174a3fa4f0c07ad033518af
tathagata218 <tathagata218@yahoo.com>
2017-08-14 19:44:20 -0500

    final Game

--- assets/game.js
+++ assets/game.js
@@ -8,10 +8,10 @@ $(function(){
     var cellImg="https://vignette2.wikia.nocookie.net/dragonball/images/6/64/Perfect_cell_by_noname37-d33a2d5.png/revision/latest?cb=20120124062858";
     var friezaImg="http://static.tvtropes.org/pmwiki/pub/images/6a1c3bbd1fa4dbd406150e869c890ef0.png";
     var gohanImg="http://img09.deviantart.net/4036/i/2017/073/3/e/gohan_ssj2__cell_saga____dragon_ball_z_by_zed_creations-db29j8g.png";
-    var cellHPCountPow="enemyHealth='100' counterattackPower='33'";
-    var gokuHPCountPow="enemyHealth='180' counterattackPower='35'";
-    var friezaHPCountPow="enemyHealth='120' counterattackPower='30'";
-    var gohanHPCountPow="enemyHealth='130' counterattackPower='32'";
+    var cellHPCountPow="enemyHealth='100' counterattackPower='12'";
+    var gokuHPCountPow="enemyHealth='180' counterattackPower='18'";
+    var friezaHPCountPow="enemyHealth='120' counterattackPower='16'";
+    var gohanHPCountPow="enemyHealth='130' counterattackPower='15'";
 //   -----------------------------------------------------------------------------------------------------------------------------
 //   -----------------------------------------------------------------------------------------------------------------------------
 //   -----------------------------------------------------------------------------------------------------------------------------  
@@ -23,19 +23,23 @@ $(function(){
         var enemyPic3="<h5>Gohan</h5><img "+imgSize+" src="+gohanImg+" alt='charecter1Img'><p id='gohanID'>HP:"+gohanHP+"</p>";
         var listofenemyChar="<div class='container-fluid'><div class='row'><div class='col-md-2 col-md-offset-3 pictures'><div "+cellHPCountPow+" class='pictures enemyImg'>"+enemyPic1+"</div></div><div class='col-md-2 pictures'><div "+friezaHPCountPow+" class='pictures enemyImg'>"+enemyPic2+"</div></div><div class='col-md-2 pictures'><div "+gohanHPCountPow+" class='pictures enemyImg'>"+enemyPic3+"</div></div></div></div>";
         var gokuattackPts=4;
+        var mainusercharHealth=180;
+         var count=0;
         //alert("click works");
         $("#charecterChoice").fadeOut();
         $("#userCharecter").append("<div id='finaluserCharecter'>"+userPic+"</div>").fadeIn();
         $("#compEnemy").html(listofenemyChar).fadeIn();
     
         $(".enemyImg").click(function(){
-            console.log("works");
+           
+            count++;           
             var thisPicture=this;
-            var newenemyHealth=0;            
+            var newenemyHealth;            
             var enemycharHealth=$(this).attr("enemyHealth");
             var counterAttack=$(this).attr("counterattackPower");
-            var mainusercharHealth=180;
+            
             console.log(newenemyHealth+" "+enemycharHealth+" "+counterAttack);
+             console.log(count);
             //$(this).fadeOut();
             $("#attackSection").append(this);
             //$('.enemyImg').off('click');
@@ -45,16 +49,16 @@ $(function(){
 
 
             $("#attackBtn").click(function(){
-                gokuattackPts=gokuattackPts+gokuattackPts;
-                mainusercharHealth=mainusercharHealth-counterAttack;
                 newenemyHealth=enemycharHealth-gokuattackPts;
+                mainusercharHealth=mainusercharHealth-counterAttack;
+                gokuattackPts=gokuattackPts+gokuattackPts;
 
                 console.log(gokuattackPts+" "+mainusercharHealth+" "+newenemyHealth);
 
                 $("#gokuhpID").html("<p>Update HP: "+mainusercharHealth+"</p>");
                 $("#displayHP").html("<p>Update HP: "+newenemyHealth+"</p>");
 
-                if(newenemyHealth<=0){
+                if(newenemyHealth<=0 && mainusercharHealth>0 && count<3){
                     console.log(newenemyHealth);
                     $(thisPicture).fadeOut();
                     $("#displayHP").fadeOut();
@@ -64,7 +68,18 @@ $(function(){
 
                         
                 }
-                
+                else if (count==3 && newenemyHealth<=0 && mainusercharHealth>0){
+
+                    $("#enemySection").empty();
+                    $("#attackSection").empty();
+                    $("#attackSection").html("<h1>You Won!!!</h1> <br>");
+                    $("#attackSection").append("<button class='btn btn-default' id='resetBtn'>Restart</button>");
+
+                    $("#resetBtn").click(function(){
+                        location.reload();
+                    });    
+
+                }
 
                 else if(mainusercharHealth<=0){
                     $("#enemySection").empty();
@@ -108,18 +123,21 @@ $(function(){
         var enemyPic2="<h5>Frieza</h5><img "+imgSize+" src="+friezaImg+" alt='charecter1Img'><p id='firezahpID'>HP:"+firezaHP+"</p>";
         var enemyPic3="<h5>Gohan</h5><img "+imgSize+" src="+gohanImg+" alt='charecter1Img'><p id='gohanID'>HP:"+gohanHP+"</p>";
         var listofenemyChar="<div class='container-fluid'><div class='row'><div class='col-md-2 col-md-offset-3 pictures'><div "+gokuHPCountPow+" class='pictures enemyImg'>"+enemyPic1+"</div></div><div class='col-md-2 pictures'><div "+friezaHPCountPow+" class='pictures enemyImg'>"+enemyPic2+"</div></div><div class='col-md-2 pictures'><div "+gohanHPCountPow+" class='pictures enemyImg'>"+enemyPic3+"</div></div></div></div>";
-        var cellattackPts=3;
+        var cellattackPts=4;
+        var mainusercharHealth=100;
+        var count=0;
         //alert("click works");
         $("#charecterChoice").fadeOut();
         $("#userCharecter").append("<div id='finaluserCharecter'>"+userPic+"</div>").fadeIn();
         $("#compEnemy").html(listofenemyChar).fadeIn();
     
         $(".enemyImg").click(function(){
+            count++;
             var thisPicture=this;
             var newenemyHealth=0;            
             var enemycharHealth=$(this).attr("enemyHealth");
             var counterAttack=$(this).attr("counterattackPower");
-            var mainusercharHealth=100;
+            
             console.log(newenemyHealth+" "+enemycharHealth+" "+counterAttack);
             //$(this).fadeOut();
             $("#attackSection").append(this);
@@ -130,25 +148,27 @@ $(function(){
 
 
             $("#attackBtn").click(function(){
-                cellattackPts=cellattackPts+cellattackPts;
-                mainusercharHealth=mainusercharHealth-counterAttack;
                 newenemyHealth=enemycharHealth-cellattackPts;
+                mainusercharHealth=mainusercharHealth-counterAttack;
+                cellattackPts=cellattackPts+cellattackPts;
 
                 console.log(cellattackPts+" "+mainusercharHealth+" "+newenemyHealth);
 
                 $("#cellhpID").html("<p>Update HP: "+mainusercharHealth+"</p>");
                 $("#displayHP").html("<p>Update HP: "+newenemyHealth+"</p>");
 
-                if(newenemyHealth<=0){
+                if(newenemyHealth<=0 && count<3 && mainusercharHealth>0){
                     console.log(newenemyHealth);
                     $(thisPicture).fadeOut();
                     $("#displayHP").fadeOut();
                     //$(".enemyImg").click();
                         $("#enemySection").fadeIn();
                         $("#compEnemy").fadeIn();
-                    
-                    if($("#enemySection").find("img").length==0){
-                        $("#enemySection").empty();
+                             
+                    }
+                else if (count==3 && newenemyHealth<=0 && mainusercharHealth>0){
+
+                    $("#enemySection").empty();
                     $("#attackSection").empty();
                     $("#attackSection").html("<h1>You Won!!!</h1> <br>");
                     $("#attackSection").append("<button class='btn btn-default' id='resetBtn'>Restart</button>");
@@ -156,9 +176,7 @@ $(function(){
                     $("#resetBtn").click(function(){
                         location.reload();
                     });    
-                        }
-                
-                    }
+                }
                 else if(mainusercharHealth<=0){
                     $("#enemySection").empty();
                     $("#attackSection").empty();
@@ -185,23 +203,26 @@ $(function(){
 //   ----------------------------------------------------------------------------------------------------------------------------- 
 
     $("#char3").click(function(){
-        var userPic="<h5>Goku</h5><img "+imgSize+" src="+friezaImg+" alt='GokuImg'><p id='gokuhpID' attackPoints=''>HP:"+firezaHP+"</p>";
-        var enemyPic1="<h5>Cell</h5><img "+imgSize+" src="+gokuImg+" alt='charecter1Img'><p id='cellhpID'>HP:"+gokuHP+"</p>";
-        var enemyPic2="<h5>Frieza</h5><img "+imgSize+" src="+cellImg+" alt='charecter1Img'><p id='firezahpID'>HP:"+cellHP+"</p>";
+        var userPic="<h5>Frieza</h5><img "+imgSize+" src="+friezaImg+" alt='GokuImg'><p id='gokuhpID' attackPoints=''>HP:"+firezaHP+"</p>";
+        var enemyPic1="<h5>Goku</h5><img "+imgSize+" src="+gokuImg+" alt='charecter1Img'><p id='cellhpID'>HP:"+gokuHP+"</p>";
+        var enemyPic2="<h5>Cell</h5><img "+imgSize+" src="+cellImg+" alt='charecter1Img'><p id='firezahpID'>HP:"+cellHP+"</p>";
         var enemyPic3="<h5>Gohan</h5><img "+imgSize+" src="+gohanImg+" alt='charecter1Img'><p id='gohanID'>HP:"+gohanHP+"</p>";
         var listofenemyChar="<div class='container-fluid'><div class='row'><div class='col-md-2 col-md-offset-3 pictures'><div "+gokuHPCountPow+" class='pictures enemyImg'>"+enemyPic1+"</div></div><div class='col-md-2 pictures'><div "+cellHPCountPow+" class='pictures enemyImg'>"+enemyPic2+"</div></div><div class='col-md-2 pictures'><div "+gohanHPCountPow+" class='pictures enemyImg'>"+enemyPic3+"</div></div></div></div>";
         var friezaattackPts=4;
+        var mainusercharHealth=120;
+        var count=0;
         //alert("click works");
         $("#charecterChoice").fadeOut();
         $("#userCharecter").append("<div id='finaluserCharecter'>"+userPic+"</div>").fadeIn();
         $("#compEnemy").html(listofenemyChar).fadeIn();
     
         $(".enemyImg").click(function(){
+            count++;
             var thisPicture=this;
             var newenemyHealth=0;            
             var enemycharHealth=$(this).attr("enemyHealth");
             var counterAttack=$(this).attr("counterattackPower");
-            var mainusercharHealth=120;
+            
             console.log(newenemyHealth+" "+enemycharHealth+" "+counterAttack);
             //$(this).fadeOut();
             $("#attackSection").append(this);
@@ -212,16 +233,16 @@ $(function(){
 
 
             $("#attackBtn").click(function(){
+                 newenemyHealth=enemycharHealth-friezaattackPts;
+                mainusercharHealth=mainusercharHealth-counterAttack;               
                 friezaattackPts=friezaattackPts+friezaattackPts;
-                mainusercharHealth=mainusercharHealth-counterAttack;
-                newenemyHealth=enemycharHealth-friezaattackPts;
 
                 console.log(friezaattackPts+" "+mainusercharHealth+" "+newenemyHealth);
 
                 $("#gokuhpID").html("<p>Update HP: "+mainusercharHealth+"</p>");
                 $("#displayHP").html("<p>Update HP: "+newenemyHealth+"</p>");
 
-                if(newenemyHealth<=0){
+                if(newenemyHealth<=0 && mainusercharHealth>0 && count<3){
                     console.log(newenemyHealth);
                     $(thisPicture).fadeOut();
                     $("#displayHP").fadeOut();
@@ -232,7 +253,17 @@ $(function(){
                         
                 }
                 
+                else if (count==3 && newenemyHealth<=0 && mainusercharHealth>0){
 
+                    $("#enemySection").empty();
+                    $("#attackSection").empty();
+                    $("#attackSection").html("<h1>You Won!!!</h1> <br>");
+                    $("#attackSection").append("<button class='btn btn-default' id='resetBtn'>Restart</button>");
+
+                    $("#resetBtn").click(function(){
+                        location.reload();
+                    });    
+                }
                 else if(mainusercharHealth<=0){
                     $("#enemySection").empty();
                     $("#attackSection").empty();
@@ -244,16 +275,7 @@ $(function(){
                     });
                 }
 
-                // if($("#enemySection").find("img").length==0){
-                //         $("#enemySection").empty();
-                //     $("#attackSection").empty();
-                //     $("#attackSection").html("<h1>You Won!!!</h1> <br>");
-                //     $("#attackSection").append("<button class='btn btn-default' id='resetBtn'>Restart</button>");
-
-                //     $("#resetBtn").click(function(){
-                //         location.reload();
-                //     });    
-                //         }
+                
 
                 //console.log(counterAttack+" "+mainusercharHelth+" "+newenemyHealth);
                 
@@ -269,12 +291,14 @@ $(function(){
 
 
     $("#char4").click(function(){
-       var userPic="<h5>Goku</h5><img "+imgSize+" src="+gohanImg+" alt='GokuImg'><p id='gokuhpID' attackPoints=''>HP:"+gohanHP+"</p>";
+       var userPic="<h5>Gohan</h5><img "+imgSize+" src="+gohanImg+" alt='GokuImg'><p id='gokuhpID' attackPoints=''>HP:"+gohanHP+"</p>";
         var enemyPic1="<h5>Cell</h5><img "+imgSize+" src="+cellImg+" alt='charecter1Img'><p id='cellhpID'>HP:"+cellHP+"</p>";
         var enemyPic2="<h5>Frieza</h5><img "+imgSize+" src="+friezaImg+" alt='charecter1Img'><p id='firezahpID'>HP:"+firezaHP+"</p>";
-        var enemyPic3="<h5>Gohan</h5><img "+imgSize+" src="+gokuImg+" alt='charecter1Img'><p id='gohanID'>HP:"+gokuHP+"</p>";
+        var enemyPic3="<h5>Goku</h5><img "+imgSize+" src="+gokuImg+" alt='charecter1Img'><p id='gohanID'>HP:"+gokuHP+"</p>";
         var listofenemyChar="<div class='container-fluid'><div class='row'><div class='col-md-2 col-md-offset-3 pictures'><div "+cellHPCountPow+" class='pictures enemyImg'>"+enemyPic1+"</div></div><div class='col-md-2 pictures'><div "+friezaHPCountPow+" class='pictures enemyImg'>"+enemyPic2+"</div></div><div class='col-md-2 pictures'><div "+gokuHPCountPow+" class='pictures enemyImg'>"+enemyPic3+"</div></div></div></div>";
         var gohanattackPts=4;
+        var mainusercharHealth=130;
+        var count=0;
         //alert("click works");
         $("#charecterChoice").fadeOut();
         $("#userCharecter").append("<div id='finaluserCharecter'>"+userPic+"</div>").fadeIn();
@@ -285,7 +309,7 @@ $(function(){
             var newenemyHealth=0;            
             var enemycharHealth=$(this).attr("enemyHealth");
             var counterAttack=$(this).attr("counterattackPower");
-            var mainusercharHealth=130;
+            
             console.log(newenemyHealth+" "+enemycharHealth+" "+counterAttack);
             //$(this).fadeOut();
             $("#attackSection").append(this);
@@ -296,16 +320,16 @@ $(function(){
 
 
             $("#attackBtn").click(function(){
-                gohanattackPts=gohanattackPts+gohanattackPts;
-                mainusercharHealth=mainusercharHealth-counterAttack;
                 newenemyHealth=enemycharHealth-gohanattackPts;
+                mainusercharHealth=mainusercharHealth-counterAttack;                
+                gohanattackPts=gohanattackPts+gohanattackPts;
 
                 console.log(gohanattackPts+" "+mainusercharHealth+" "+newenemyHealth);
 
                 $("#gokuhpID").html("<p>Update HP: "+mainusercharHealth+"</p>");
                 $("#displayHP").html("<p>Update HP: "+newenemyHealth+"</p>");
 
-                if(newenemyHealth<=0){
+                if(newenemyHealth<=0 && mainusercharHealth>0 && count<3){
                     console.log(newenemyHealth);
                     $(thisPicture).fadeOut();
                     $("#displayHP").fadeOut();
@@ -316,6 +340,17 @@ $(function(){
                         
                 }
                 
+                else if (count==3 && newenemyHealth<=0 && mainusercharHealth>0){
+
+                    $("#enemySection").empty();
+                    $("#attackSection").empty();
+                    $("#attackSection").html("<h1>You Won!!!</h1> <br>");
+                    $("#attackSection").append("<button class='btn btn-default' id='resetBtn'>Restart</button>");
+
+                    $("#resetBtn").click(function(){
+                        location.reload();
+                    });    
+                }
 
                 else if(mainusercharHealth<=0){
                     $("#enemySection").empty();
@@ -328,16 +363,7 @@ $(function(){
                     });
                 }
 
-                // if($("#enemySection").find("img").length==0){
-                //         $("#enemySection").empty();
-                //     $("#attackSection").empty();
-                //     $("#attackSection").html("<h1>You Won!!!</h1> <br>");
-                //     $("#attackSection").append("<button class='btn btn-default' id='resetBtn'>Restart</button>");
-
-                //     $("#resetBtn").click(function(){
-                //         location.reload();
-                //     });    
-                //         }
+            
 
                 //console.log(counterAttack+" "+mainusercharHelth+" "+newenemyHealth);
                 
